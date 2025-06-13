f79ebc7d3029bb79cda8d16bfd89dedad5d5bff0
Matt Antonelli <contact@raelys.com>
2025-06-12 23:07:41 -0400

    Improve organization with export module

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,20 @@
-use csv::Writer;
 use std::error::Error;
 use std::path::Path;
 
 use ironworks::{
     Ironworks,
-    excel::{Excel, Field, Language},
+    excel::{Excel, Language},
     sqpack::{Install, SqPack},
 };
 
-fn field_to_string(field: Field) -> String {
-    return match field {
-        // TODO: Figure out formatting for complex strings (e.g. descriptions, tooltips)
-        Field::String(value) => value.format().unwrap(),
-        Field::Bool(value) => {
-            if value {
-                String::from("True")
-            } else {
-                String::from("False")
-            }
-        }
-        Field::I8(value) => value.to_string(),
-        Field::I16(value) => value.to_string(),
-        Field::I32(value) => value.to_string(),
-        Field::I64(value) => value.to_string(),
-        Field::U8(value) => value.to_string(),
-        Field::U16(value) => value.to_string(),
-        Field::U32(value) => value.to_string(),
-        Field::U64(value) => value.to_string(),
-        Field::F32(value) => value.to_string(),
-    };
-}
+mod export;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let path = Path::new("D:\\SquareEnix\\FINAL FANTASY XIV - A Realm Reborn");
     let ironworks = Ironworks::new().with_resource(SqPack::new(Install::at(path)));
-
-    let sheet_name = "Mount";
     let excel = Excel::new(ironworks).with_default_language(Language::English);
-    let sheet = excel.sheet(sheet_name)?;
-    let columns = sheet.columns()?;
 
-    // Sort by offset vs default sort by index
-    // columns.sort_by_key(|column| column.offset);
-
-    let path = format!("output/{}.csv", sheet_name);
-    let mut writer = Writer::from_path(path)?;
-
-    for row in sheet.into_iter() {
-        let row = &row?;
-        let mut data: Vec<String> = Vec::new();
-
-        // TODO: Support row_id.subrow_id
-        data.push(row.row_id().to_string());
-
-        // TODO: We need to reference the field by the offset, not index, in order to match EXDSchema
-        for i in 0..columns.len() {
-            data.push(field_to_string(row.field(i)?));
-        }
-
-        writer.serialize(data)?;
-    }
-
-    writer.flush()?;
+    export::sheet(&excel, "Mount")?;
 
     return Ok(());
 }
