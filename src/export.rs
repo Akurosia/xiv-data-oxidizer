e26462df0ea8cc7eaf892a12ea48f135469ec5d5
Matt Antonelli <contact@raelys.com>
2025-06-15 21:12:57 -0400

    Retrieve fields in offset order
    The data now lines up properly with the EXDSchema field names.

--- src/export.rs
+++ src/export.rs
@@ -2,16 +2,17 @@ use csv::Writer;
 use std::error::Error;
 
 use ironworks::excel::{Excel, Field};
+use ironworks::file::exh::ColumnDefinition;
 
 use crate::exd_schema;
 
 pub fn sheet(excel: &Excel, sheet_name: &str) -> Result<(), Box<dyn Error>> {
     let field_names = exd_schema::field_names(sheet_name)?;
     let sheet = excel.sheet(sheet_name)?;
-    let columns = sheet.columns()?;
 
-    // Sort by offset vs default sort by index
-    // columns.sort_by_key(|column| column.offset);
+    // Sort by offset to align with EXDSchema column order
+    let mut columns = sheet.columns()?;
+    columns.sort_by_key(|column| column.offset);
 
     let path = format!("output/{}.csv", sheet_name);
     let mut writer = Writer::from_path(path)?;
@@ -26,9 +27,14 @@ pub fn sheet(excel: &Excel, sheet_name: &str) -> Result<(), Box<dyn Error>> {
         // TODO: Support row_id.subrow_id
         data.push(row.row_id().to_string());
 
-        // TODO: We need to reference the field by the offset, not index, in order to match EXDSchema
-        for i in 0..columns.len() {
-            data.push(field_to_string(row.field(i)?));
+        for column in columns.iter() {
+            let specifier = ColumnDefinition {
+                kind: column.kind,
+                offset: column.offset,
+            };
+            let field = row.field(&specifier)?;
+
+            data.push(field_to_string(&field));
         }
 
         writer.serialize(data)?;
@@ -39,12 +45,12 @@ pub fn sheet(excel: &Excel, sheet_name: &str) -> Result<(), Box<dyn Error>> {
     return Ok(());
 }
 
-fn field_to_string(field: Field) -> String {
+fn field_to_string(field: &Field) -> String {
     return match field {
         // TODO: Figure out formatting for complex strings (e.g. descriptions, tooltips)
         Field::String(value) => value.format().unwrap(),
         Field::Bool(value) => {
-            if value {
+            if *value {
                 String::from("True")
             } else {
                 String::from("False")
